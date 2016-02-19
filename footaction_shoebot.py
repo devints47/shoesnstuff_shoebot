4f5fe040cad9795d43c08a8d680da26ec2230371
Devin Singh <devints47@gmail.com>
2016-02-19 04:34:07 -0500

    Removed leftover print statement

--- footaction_shoebot.py
+++ footaction_shoebot.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from random import randint
 
 
-cvc = ''		# Enter your CVC number
+cvc = '102'		# Enter your CVC number
 size = '10.0'	# Enter your size. Ex: 08.5, 09.0, 10.5, 12.5
 
 footaction_url = 'http://www.footaction.com/product/model:254106/sku:BB5350/adidas-originals-yeezy-boost-350-mens/?cm=#sku=BB5350&size=' + size
@@ -41,7 +41,6 @@ while x is 0:
 		# driver.find_element_by_id('orderSubmit').click()
 		x = 1
 	except Exception as e:
-		print e
 		attempt_number = attempt_number + 1
 		print "Couldn't find the button. Refreshing...\n"
 		driver.refresh()
